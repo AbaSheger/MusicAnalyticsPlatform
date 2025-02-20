85a4e277c9e301b0fa593d85b8f9d2e8734e5c1f
AbaSheger <111607144+AbaSheger@users.noreply.github.com>
2025-02-20 10:13:59 +0100

    Commit changes

---
+++ user-tracking-service/src/main/java/com/example/usertracking/UserTrackingServiceApplication.java
@@ -0,0 +1,7 @@
+package com.example.usertracking;
+
+public class UserTrackingServiceApplication {
+    public static void main(String[] args) {
+        System.out.println("User Tracking Service is running!");
+    }
+}
