ee41984475534ebeae90e6df4364cc77df333e41
AbaSheger <111607144+AbaSheger@users.noreply.github.com>
2025-03-18 16:11:35 +0100

    feat: remove SelfPingService and update environment variables in Docker Compose

--- eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -3,11 +3,9 @@ package com.example.eurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableEurekaServer
-@EnableScheduling
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
